8682155ef75af65458bc8335787f32271ad808db
Tarmeesee Daoh <tarmizee.smp01@gmail.com>
2025-02-22 00:48:28 +0700

    change app

--- src/App.java
+++ src/App.java
@@ -1,4 +1,33 @@
-
+//import javafx.application.Application;
+//import javafx.geometry.Insets;
+//import javafx.scene.Scene;
+//import javafx.scene.layout.Background;
+//import javafx.scene.layout.BackgroundFill;
+//import javafx.scene.layout.CornerRadii;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.paint.Color;
+//import javafx.stage.Stage;
+//
+//public class App extends Application {
+//
+//	@Override
+//	public void start(Stage stage) throws Exception {
+//		// TODO Auto-generated method stub
+//		HBox root = new HBox();
+//		root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+//		Scene scene = new Scene(root, 900, 650);
+//		
+//		root.setOnMouseClicked((e) -> {
+//			double x = e.getX();
+//			double y = e.getY();
+//			System.out.println("x = " + x + ", y = " + y);
+//		});
+//		
+//		stage.setScene(scene);
+//		stage.setTitle("SaaYoong");
+//		stage.show();
+//		
+//	}
 
 
 import javafx.application.Application;
@@ -12,6 +41,7 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         // Load the image
         Image backgroundImage = new Image("900X650 BG.png"); // Change the path
+
         // Create a BackgroundImage
         BackgroundImage background = new BackgroundImage(
             backgroundImage,
@@ -20,7 +50,6 @@ public class App extends Application {
             BackgroundPosition.CENTER,
             new BackgroundSize(100, 100, true, true, true, false)
         );
-
 
         // Create a Pane and set the background
         Pane root = new Pane();
@@ -38,3 +67,9 @@ public class App extends Application {
     }
 }
 
+//	
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+//
+//}
