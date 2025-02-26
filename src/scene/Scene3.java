52a674c5b37a0d3ce98a0a38fad01f537a0d2393
Ethan Winter <thanpisit1403@gmail.com>
2025-02-27 02:44:33 +0700

    work in process

--- src/scene/Scene3.java
+++ src/scene/Scene3.java
@@ -18,7 +18,8 @@ public class Scene3 extends ScenePane {
 
 	private int sceneGuider = 0;
 	private ArrayList<Background> backgroundList;
-	private ImageObject bg1_sink, crack, faucet, oneghost, optionalghost, text, tryMe, urinal;
+	private ImageObject bg1_sink, oneghost, text, tryMe, urinal;
+	private NoisyObject crack, faucet, optionalghost;
 	public Scene3() {
 		super("scene3/BG_scene3_1.png");
 		// TODO Auto-generated constructor stub
@@ -28,13 +29,12 @@ public class Scene3 extends ScenePane {
 		for (String file: bg) {
 			backgroundList.add(createBackground("scene3/BG_scene3_2_" + file + ".png"));
 		}
-//		back
-//		this.overall = new Scene(this, 900, 650);
-		bg1_sink = new ImageObject("scene3/object/bg1_sink.png");
-		crack = new NoisyObject("scene3/object/crack.png", "scene3/sound/glass-breaking.mp3");
-		faucet = new NoisyObject("scene3/object/faucet.png", "scene3/sound/open_faucet1.mp3");
+		
+		bg1_sink = new NoisyObject("scene3/object/bg1_sink.png", "scene3/sound/open_faucet2.mp3", 500);
+		crack = new NoisyObject("scene3/object/crack.png", "scene3/sound/glass-breaking.mp3", 500);
+		faucet = new NoisyObject("scene3/object/faucet.png", "scene3/sound/open_faucet1.mp3", 500);
 		oneghost = new ImageObject("scene3/object/oneghost.png");
-		optionalghost = new ImageObject("scene3/object/optionalghost.png");
+		optionalghost = new NoisyObject("scene3/object/optionalghost.png", "scene3/sound/ghostwomenhaha.mp3");
 		text = new ImageObject("scene3/object/text.png");
 		tryMe = new ImageObject("scene3/object/tryme.png");
 		urinal = new ImageObject("scene3/object/urinal.png");
@@ -60,6 +60,7 @@ public class Scene3 extends ScenePane {
 		startTextFade();
 		
 		bg1_sink.setOnMouseClicked(event -> {
+			bg1_sink.onClick();
 			this.getChildren().remove(bg1_sink);
 			this.setBackground(backgroundList.get(1));
 			urinal.open();
@@ -74,6 +75,7 @@ public class Scene3 extends ScenePane {
 		});
 		
 		faucet.setOnMouseClicked(event -> {
+			faucet.onClick();
 			this.getChildren().remove(faucet);
 			this.setBackground(backgroundList.get(3));
 			oneghost.open();
@@ -92,18 +94,36 @@ public class Scene3 extends ScenePane {
 				this.getChildren().remove(tryMe);
 				this.setBackground(backgroundList.get(5));
 				crack.open();
+				crack.onClick();
 				sceneGuider = 5;
 			}
 		});
 		
 		crack.setOnMouseClicked(event -> {
+			crack.onClick();
 			if (sceneGuider == 5) {
-				optionalghost.open();
+//				optionalghost.open();
+//				optionalghost.onClick();
+				this.setBackground(backgroundList.get(6));
 				sceneGuider = 6;
-				
+			} else if (sceneGuider == 6) { 
+				optionalghost.open();
+				optionalghost.onClick();
+				optionalghost.onClick();
+				optionalghost.onClick();
+				FadeTransition ghostFadeOut = new FadeTransition(Duration.seconds(0.001), optionalghost) ;
+	            ghostFadeOut.setDelay(Duration.millis(1000));
+	            ghostFadeOut.setFromValue(1.0);
+	            ghostFadeOut.setToValue(0.0);
+	            ghostFadeOut.setOnFinished(subevent -> {
+	            	this.getChildren().remove(optionalghost);
+	            	this.getChildren().remove(crack);
+	            });
+	            ghostFadeOut.play();
+	            
 				new Thread(() -> {
 	        		try {
-	                    Thread.sleep(3000); // Hold for 3 seconds
+	                    Thread.sleep(5000); // Hold for 3 seconds
 	                } catch (InterruptedException e) {
 	                    Thread.currentThread().interrupt();
 	                }
--- src/scene/Scene5.java
+++ src/scene/Scene5.java
@@ -2,7 +2,10 @@ package scene;
 
 import component.ImageObject;
 import component.NoisyObject;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.layout.Background;
+import javafx.util.Duration;
 
 /*
 Scene 5
@@ -23,6 +26,7 @@ Scene 5
 
 public class Scene5 extends ScenePane {
 
+	private int sceneGuider = 0;
 	private ImageObject[] ties, ps, comsmile, projector;
 	private ImageObject blackscreen, keyboard, monitor, mouse, timetohavefun, welcomestupid;
 	
@@ -43,6 +47,8 @@ public class Scene5 extends ScenePane {
 		for (int i=0; i<7; i++) {
 			ties[i] = new ImageObject("scene5/object/tie" + (i+1) +".png");
 			ps[i] = new ImageObject("scene5/object/p" + (i+1) +".png");
+			ties[i].close();
+			ps[i].close();
 		}
 		
 		comsmile[0] = new ImageObject("scene5/object/comsmile_1.png");
@@ -57,13 +63,91 @@ public class Scene5 extends ScenePane {
 		timetohavefun = new ImageObject("scene5/object/timetohavefun.png");
 		welcomestupid = new ImageObject("scene5/object/welcomestupid.png");
 		
+		comsmile[0].close();
+		comsmile[1].close();
+		comsmile[2].close();
+		projector[0].close();
+		projector[1].close();
+		blackscreen.close();
+		keyboard.close();
+		monitor.close();
+		mouse.close();
+		timetohavefun.close();
+		welcomestupid.close();
+		
+		this.getChildren().addAll(
+				ties[0], ties[1], ties[2], ties[3], ties[4], ties[5], ties[6],
+				ps[0], ps[1], ps[2], ps[3], ps[4], ps[5], ps[6],
+				comsmile[0], comsmile[1], comsmile[2], 
+				projector[0], projector[1], 
+				blackscreen, keyboard, monitor, mouse,
+				timetohavefun, welcomestupid);
+		
 //		this.getChildren().addAll(ps);
-		NoisyObject temp = new NoisyObject("scene5/object/blackscreen.png", getAccessibleHelp());
+//		NoisyObject temp = new NoisyObject("scene5/object/blackscreen.png", getAccessibleHelp());
+		setBackground(backgrounds[0]);
 		
 		this.setOnMouseClicked(event -> {
-				
-				this.setBackground(backgrounds[(order++)%3]);
-				
+			if (sceneGuider == 0) {
+				projector[1].open();
+				sceneGuider = 1;
+			} 
+		});
+		
+		projector[1].setOnMouseClicked(event -> {
+			if (sceneGuider == 1) {
+				monitor.open();
+				sceneGuider = 2;
+			}
+		});
+		
+		monitor.setOnMouseClicked(event -> {
+			if (sceneGuider == 2) {
+				setBackground(backgrounds[2]);
+				sceneGuider = 3;
+			} 
+//			else if (sceneGuider != 3) {
+//				return;
+//			}
+//			
+//			// Show all comsmile images (they will flicker)
+//		    comsmile[0].open();
+//		    comsmile[1].open();
+//		    comsmile[2].open();
+//
+//		    // Create a timeline to switch images quickly
+//		    Timeline glitchEffect = new Timeline(
+//		        new KeyFrame(Duration.millis(100), e -> {
+//		            comsmile[0].setVisible(true);
+//		            comsmile[1].setVisible(false);
+//		            comsmile[2].setVisible(false);
+//		        }),
+//		        new KeyFrame(Duration.millis(200), e -> {
+//		            comsmile[0].setVisible(false);
+//		            comsmile[1].setVisible(true);
+//		            comsmile[2].setVisible(false);
+//		        }),
+//		        new KeyFrame(Duration.millis(300), e -> {
+//		            comsmile[0].setVisible(false);
+//		            comsmile[1].setVisible(false);
+//		            comsmile[2].setVisible(true);
+//		        })
+//		    );
+//		    
+//		    glitchEffect.setCycleCount(5);
+//		    glitchEffect.setOnFinished(e -> {
+//		        // Hide comsmile images after glitching
+//		        comsmile[0].close();
+//		        comsmile[1].close();
+//		        comsmile[2].close();
+//
+//		        // Switch to black screen
+//		        setBackground(backgrounds[1]); // Assuming backgrounds[1] is BG_scene5_black
+//		    });
+//
+//		    // Start the glitch animation
+//		    glitchEffect.play();
+//		    sceneGuider = 4;
 		});
 	}
 
